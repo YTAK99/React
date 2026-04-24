55492b8647760206371760c5a41f45824bee1c4f
YTAK99 <ytzzang99@gmail.com>
2026-04-24 18:41:05 +0900

    first commit

---
+++ src/component/Body.jsx
@@ -0,0 +1,11 @@
+import "./Body.css";
+
+function Body({name, location}) {
+    console.log(name, location);
+    return ( 
+        <div className="body">
+        {name}은 {location}에 거주합니다.
+        </div>
+    );
+}
+export default Body;
