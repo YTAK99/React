e4bf3bd5a71cc5c78b3c100061e486b2dc56ecaf
YTAK99 <ytzzang99@gmail.com>
2026-04-24 18:45:00 +0900

    gogo

---
+++ 0424/MyHeader.js
@@ -0,0 +1,7 @@
+function MyHeader() {
+    return (
+        <header>
+            <h1>안녕하세요 수정된 이수정수정입니다.</h1>
+        </header>
+    );
+}
---
+++ CHAPTER4/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>똑.똑. 안녕하세요? <br/> 누구세요?</h2>
+      </div>
+  )
+}
+
+export default App
