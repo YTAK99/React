48cc3414c1a884b981389fc019fcc1337e2565da
YTAK99 <ytzzang99@gmail.com>
2026-04-28 12:49:35 +0900

    0428

--- CHAPTER5/src/component/Body.jsx
+++ CHAPTER5/src/component/Body.jsx
@@ -1,32 +1,30 @@
 import "./Body.css";
-import { useState } from "react";
-
-function Viewer() {
-    console.log("viewer component update!");
-    return <div>Viewer</div>;
-}
+import { useState, useRef } from "react";
 
 function Body() {
-    const [number, setNumber] = useState(0);
-    const onIncrease = () => {
-        setNumber(number + 1);
-    };
-    const onDecrease = () => {
-        setNumber(number-1);
-    };
-    return (
-        <div>
-            <h2>{number}</h2>
-            <Viewer number={number} />
+    const [text, setText] = useState("");
+    const textRef = useRef();
 
-            <div>
-                <button onClick={onDecrease}>-</button>
-                <button onClick={onIncrease}>+</button>
-            </div>
+    const handleOnChange = (e) => {
+        setText(e.target.value);
+    };
+    const handleOnClick = () => {
+        if (text.length < 5) {
+            textRef.current.focus();
+        }
+        else {
+            alert(text);
+            setText("");
+        }
+    };
+
+    return (
+        <div className="body">
+            <input ref={textRef} value={text} onChange={handleOnChange} />
+            <button onClick={handleOnClick}>작성완료</button>
         </div>
     );
 }
-
 export default Body;
 
 
