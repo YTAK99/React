e7cfd954f4c015bbc3d9cbfc905bf1ea9fb2ec19
YTAK99 <ytzzang99@gmail.com>
2026-05-06 18:02:18 +0900

    0506

---
+++ Project3_EmotionDiary/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import Editor from "../component/Editor";
+
+const Home = () => {
+    return (
+        <div>
+            <Editor 
+                onSubmit={() => {
+                    alert("작성 완료 버튼을 클릭했음");
+                }}
+            />
+        </div>
+    );
+};
+export default Home;
+
+
+
+// import Button from "../component/Button";
+// import Header from "../component/Header";
+
+// const Home = () => {
+//     return(
+//         <div>
+//             <Header
+//                 title={"Home"}
+
+//                 leftChild={
+//                     <Button
+//                         type="positive"
+//                         text={"긍정 버튼"}
+//                         onClick={() => {
+//                             alert("positive button");
+//                         }} 
+//                     />             
+//                 }
+            
+//                 rightChild={
+//                     <Button
+//                         type="negative"
+//                         text={"부정 버튼"}
+//                         onClick={() => {
+//                             alert("negative button");
+//                         }} 
+//                     />
+//                 }
+//             />
+//         </div>
+//     );
+// };
+// export default Home;
