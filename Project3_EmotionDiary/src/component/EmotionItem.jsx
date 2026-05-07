1a3bb58397b98bf7751c160c73e42ef7253f93a3
YTAK99 <ytzzang99@gmail.com>
2026-05-07 17:48:17 +0900

    0507

--- Project3_EmotionDiary/src/component/EmotionItem.jsx
+++ Project3_EmotionDiary/src/component/EmotionItem.jsx
@@ -6,7 +6,12 @@ const EmotionItem = ({ id, img, name, onClick, isSelected }) => {
         onClick(id);
     };
     return (
-        <div className="EmotionItem" onClick={handleOnClick}>
+        <div 
+            className={`EmotionItem ${
+                isSelected ? `EmotionItem_on_${id}` : `EmotionItem_off`
+            }`} 
+            onClick={handleOnClick}>
+
             <img alt={`emotion${id}`} src={img} />
             <span>{name}</span>
         </div>
--- Project3_EmotionDiary/src/pages/Home.jsx
+++ Project3_EmotionDiary/src/pages/Home.jsx
@@ -1,50 +1,46 @@
-import Editor from "../component/Editor";
+import { useState, useContext, useEffect } from "react";
+import { DiaryStateContext } from "../App";
+import { getMonthRangeByDate } from "../util";
+import Button from "../component/Button";
+import Header from "../component/Header";
+import DiaryList from "../component/DiaryList";
 
 const Home = () => {
+    const data = useContext(DiaryStateContext);
+    const [pivotDate, setPivoDate] = useState(new Date());
+    const [filteredData, setFilteredData] = useState([]);
+    const headerTitle = `${pivotDate.getFullYear()}년
+                         ${pivotDate.getMonth() + 1}월`;
+    
+    const onIncreaseMonth = () => {
+        setPivoDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1));
+    };
+
+    const onDecreaseMonth = () => {
+        setPivoDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1));
+    };
+
+    useEffect(() => {
+        if (data.length >= 1) {
+            const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+            setFilteredData(
+                data.filter(
+                    (it) => beginTimeStamp <= it.date <= endTimeStamp
+                )
+            );
+        }
+        else { setFilteredData([]); }
+    }, [data, pivotDate]);
+
     return (
         <div>
-            <Editor 
-                onSubmit={() => {
-                    alert("작성 완료 버튼을 클릭했음");
-                }}
+            <Header
+                title={headerTitle}
+                leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>}
+                rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}
             />
+            <DiaryList data={filteredData} />
         </div>
     );
 };
 export default Home;
-
-
-
-// import Button from "../component/Button";
-// import Header from "../component/Header";
-
-// const Home = () => {
-//     return(
-//         <div>
-//             <Header
-//                 title={"Home"}
-
-//                 leftChild={
-//                     <Button
-//                         type="positive"
-//                         text={"긍정 버튼"}
-//                         onClick={() => {
-//                             alert("positive button");
-//                         }} 
-//                     />             
-//                 }
-            
-//                 rightChild={
-//                     <Button
-//                         type="negative"
-//                         text={"부정 버튼"}
-//                         onClick={() => {
-//                             alert("negative button");
-//                         }} 
-//                     />
-//                 }
-//             />
-//         </div>
-//     );
-// };
-// export default Home;
